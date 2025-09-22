24d2af198f31df56d1750e71210b533b30f799cd
hiranya.sarma <hiranya.sarma@benevity.com>
2025-09-22 22:35:00 +0530

    Inner class added

---
+++ src/com/eazybytes/app/MyOuterClass.java
@@ -0,0 +1,9 @@
+package com.eazybytes.app;
+
+public class MyOuterClass {
+    public static class MyInnerClass{
+        public void display(){
+            System.out.println("Inside from MyInnerClass");
+        }
+    }
+}
