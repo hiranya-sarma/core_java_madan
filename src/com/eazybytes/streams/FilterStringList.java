0074345e554acab823bd7e063512196b45458c1e
hiranya.sarma <hiranya.sarma@benevity.com>
2026-02-20 14:07:26 +0530

    Stream filter added from MAC

---
+++ src/com/eazybytes/streams/FilterStringList.java
@@ -0,0 +1,25 @@
+package com.eazybytes.streams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FilterStringList {
+    public static void main(String[] args) {
+
+        List<String> fruits = Arrays.asList("Apple", "Banana", "Orange", "Mango");
+
+        List<String> filteredFruits = new ArrayList<>();
+
+        for(String fruit : fruits){
+            if(fruit.equals("Banana")){
+                filteredFruits.add(fruit);
+            }
+        }
+        System.out.println(filteredFruits);
+
+        //Using Stream API
+        List<String> filteredFruits1 = fruits.stream().filter(fruit -> fruit.equals("Banana")).toList();
+        System.out.println(filteredFruits1);
+    }
+}
