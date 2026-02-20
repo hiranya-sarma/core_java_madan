cd943373a20f739f84527d9ec5e7b9a7e10eec98
hiranya.sarma <hiranya.sarma@benevity.com>
2026-02-21 00:44:13 +0530

    Stream map added from MAC

--- src/com/eazybytes/streams/MapCustomObjectExample.java
+++ src/com/eazybytes/streams/MapCustomObjectExample.java
@@ -34,6 +34,41 @@ class User{
     }
 }
 
+class UserDTO{
+    private int id;
+    private String username;
+    private String email;
+
+    UserDTO(int id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    @Override
+    public String toString() {
+        return "UserDTO {id=" + id + ", username=" + username + ", email=" + email + "}";
+    }
+
+}
+
 public class MapCustomObjectExample {
     public static void main(String[] args) {
 
@@ -42,12 +77,16 @@ public class MapCustomObjectExample {
         users.add(new User(2, "Ravi", "123456", "ravi@example.com"));
         users.add(new User(3, "Rajesh", "123456", "rajesh@example.com"));
         users.add(new User(4, "Rahul", "123456", "rahul@example.com"));
-
+        /*
         List<String> emailIds = users.stream()
                 .peek(user -> System.out.println("Before map: "+user.toString()))
                 .map(user ->user.getEmail())
                 .peek(user -> System.out.println("After map: "+user.toString()))
                 .toList();
         System.out.println(emailIds);
+         */
+
+        List<UserDTO> userDTOS = users.stream().map( user -> new UserDTO(user.getId(), user.getUsername(), user.getEmail())).toList();
+        userDTOS.forEach(elememt -> System.out.println(elememt.toString()));
     }
 }
