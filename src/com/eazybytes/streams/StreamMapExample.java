91cdd13c7c7b2aa3b2a0328edb1c258b14f89295
hiranya.sarma <hiranya.sarma@benevity.com>
2026-02-20 23:04:31 +0530

    Stream map added from MAC

---
+++ src/com/eazybytes/streams/StreamMapExample.java
@@ -0,0 +1,24 @@
+package com.eazybytes.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamMapExample {
+    public static void main(String[] args) {
+        //source
+        List<String> fruits = Arrays.asList("Apple", "Banana", "Orange", "Mango");
+
+        Stream<String> fruitStream = fruits.stream();
+
+        List<String> fruitList= fruitStream
+                .peek(e-> System.out.println("Before mapping: "+e))
+                .map(fruit -> fruit.toUpperCase())
+                .peek(e-> System.out.println("After mapping: "+e))
+                .toList();
+
+        System.out.println(fruitList);
+
+
+    }
+}
