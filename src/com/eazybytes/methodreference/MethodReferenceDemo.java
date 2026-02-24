260612f055ef54949dd6b3e82d5980984bfba05a
hiranya.sarma <hiranya.sarma@benevity.com>
2026-02-24 20:26:12 +0530

    Stream map added from MAC

--- src/com/eazybytes/methodreference/MethodReferenceDemo.java
+++ src/com/eazybytes/methodreference/MethodReferenceDemo.java
@@ -1,5 +1,6 @@
 package com.eazybytes.methodreference;
 
+import java.util.*;
 import java.util.function.Function;
 
 @FunctionalInterface
@@ -32,9 +33,29 @@ public class MethodReferenceDemo {
         MethodReferenceDemo methodReferenceDemo = new MethodReferenceDemo();
         //lambda expression
         Printable printable = (msg) -> methodReferenceDemo.display(msg);
-        printable.print("hello world");
+        printable.print("hello world 1");
 
+        //3
+        Function<String, String> strToUpper = (input) -> input.toUpperCase();
+        System.out.println(strToUpper.apply("hello world 2"));
+        //using method reference
+        Function<String, String> strToUpper1 = String::toUpperCase;
+        System.out.println(strToUpper1.apply("hello world 3"));
 
+        String[] strArray = {"A", "E", "I", "O", "U", "a", "e", "i", "o", "u"};
+
+        Arrays.sort(strArray, (s1, s2) -> s1.compareToIgnoreCase(s2));
+        Arrays.sort(strArray, String::compareToIgnoreCase);
+
+        //4 reference to a constructor
+        List<String> fruits = new ArrayList<>(Arrays.asList("Apple", "Banana", "Orange", "Mango", "Banana"));
+        System.out.println(fruits);
+        Function<List<String>, Set<String>> distinctFruits = (fruitList) -> new HashSet<>(fruitList);
+        System.out.println(distinctFruits.apply(fruits));
+
+        //method reference
+        Function<List<String>, Set<String>> setMethodReference = HashSet::new;
+        System.out.println(setMethodReference.apply(fruits));
 
 
 
