376580fc0edc7ef0560cc6695eef69d5225cfc15
hiranya <hiranya_oxford@yopmail.com>
2026-03-01 00:43:05 +0530

    Generic added from Ubuntu

---
+++ src/com/eazybytes/collection/CreateArrayListFromCollection.java
@@ -0,0 +1,17 @@
+package com.eazybytes.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CreateArrayListFromCollection {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(6,7,8,9,10));
+
+        List<Integer> list3 = new ArrayList<>(list1);
+        list3.addAll(list2);
+        System.out.println(list3);
+    }
+}
