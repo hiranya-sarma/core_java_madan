22b9d1c793323a7e93511b32f1b759ca90ae426e
hiranya <hiranya_oxford@yopmail.com>
2026-04-16 11:42:46 +0530

    Locking added from Ubuntu

--- src/com/eazybytes/collection/treemap/CreateTreeMapExample.java
+++ src/com/eazybytes/collection/treemap/CreateTreeMapExample.java
@@ -12,5 +12,13 @@ public class CreateTreeMapExample {
         fileExtensions.put("golang",".go");
         fileExtensions.put("java",".java");
         System.out.println(fileExtensions);
+
+        SortedMap<String, Integer> map1 = new TreeMap<>();
+        map1.put("One", 1);
+        map1.put("Two", 2);
+        map1.put("Three", 3);
+        map1.put("Four", 4);
+        map1.put("Five", 5);
+        System.out.println(map1);
     }
 }
