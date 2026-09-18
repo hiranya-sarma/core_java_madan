03cb7841a4922082bddaaa3a60e0096d04aebf29
hiranya <hiranya_oxford@yopmail.com>
2026-09-18 14:44:13 +0530

    LL section completed from udemy

---
+++ src/com/scottdsa/doublylinkedlist/Main.java
@@ -0,0 +1,10 @@
+package com.scottdsa.doublylinkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList(11);
+
+        //dll.printList();
+        dll.printAll();
+    }
+}
