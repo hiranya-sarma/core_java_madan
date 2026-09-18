f687aa7232c1b468535f6ac508e2cc5cc2bcc469
hiranya-sarma <hiranya.kahuna@gmail.com>
2026-09-18 21:07:24 +0530

    Stack class added

---
+++ src/com/scottdsa/stack/Main.java
@@ -0,0 +1,8 @@
+package com.scottdsa.stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack myStack = new Stack(1);
+        myStack.printStack();
+    }
+}
---
+++ src/com/scottdsa/stack/Stack.java
@@ -0,0 +1,28 @@
+package com.scottdsa.stack;
+
+public class Stack {
+    private Node top;
+    private int height;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.print(temp.value + " ");
+            temp = temp.next;
+        }
+    }
+}
